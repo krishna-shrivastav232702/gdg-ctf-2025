dc13f503b84bb809e06dee5832c74be040906da7
krishna <krishnashrivastava23@gmail.com>
2025-03-16 18:49:25 +0530

    feat:Add Database connection , user model and submission model

---
+++ src/db/db.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL as string;
+
+if (!MONGO_URL) {
+    throw new Error("Please define the mongo db url");
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connectToDatabase;
