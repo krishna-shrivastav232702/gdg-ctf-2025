bce1c53d20c67ac8fa8120eded46656d94cea99f
SwathiJambagi <1nt23is228.swathi@nmit.ac.in>
2025-03-20 19:33:02 +0530

    added challenge_1 ui

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -4,46 +4,7 @@ import "@fontsource/poppins";
 import React from "react";
 
 import { HoverEffect } from "@/components/ui/card-hover-effect";
-import Image from "next/image";
-import Link from "next/link";
-import { FaUserCircle } from "react-icons/fa";
-
-const Navbar: React.FC = () => (
-  <nav className="bg-transparent text-white p-4 flex items-center justify-between relative z-10 font-[Poppins]">
-    <div className="flex items-center space-x-2">
-      <Image
-        alt="logo"
-        src={require("../gdgnmit_logo.webp")}
-        width={50}
-        height={50}
-      />
-    </div>
-    <div className="flex items-center space-x-6">
-      <Link
-        href="/challenge1"
-        className="hover:text-gray-400 cursor-pointer font-[Poppins]"
-      >
-        Challenge 1
-      </Link>
-      <Link
-        href="/challenge2"
-        className="hover:text-gray-400 cursor-pointer font-[Poppins]"
-      >
-        Challenge 2
-      </Link>
-      <Link
-        href="/challenge3"
-        className="hover:text-gray-400 cursor-pointer font-[Poppins]"
-      >
-        Challenge 3
-      </Link>
-      <div className="flex items-center space-x-2 font-[Poppins]">
-        <FaUserCircle className="text-2xl" />
-        <span>John Doe</span>
-      </div>
-    </div>
-  </nav>
-);
+import { Navbar } from "@/components/ui/navbar";
 
 const DATA = [
   {
@@ -64,16 +25,16 @@ const DATA = [
 ];
 
 const Challenges: React.FC = () => (
-  
-    <div className="min-h-screen bg-black text-white font-[Poppins]">
-      <Navbar />
-      <main className="px-56 py-10 text-center font-[Poppins]">
-        <h1 className="text-3xl font-bold mb-8 font-[Poppins]">Challenges</h1>
-        <HoverEffect items={DATA} />
-      </main>
-      <BackgroundBeams />
-    </div>
-  
+  <div className="min-h-screen bg-black text-white font-[Poppins]">
+    <Navbar />
+    <main className="px-56 py-10 text-center font-[Poppins]">
+      <h1 className="text-3xl font-bold mb-8 font-[Poppins] mt-24">
+        Challenges
+      </h1>
+      <HoverEffect items={DATA} />
+    </main>
+    <BackgroundBeams />
+  </div>
 );
 
 export default Challenges;
