9d7f8553f82040a8515abcde4c1f1a7f1c5cc014
krishna <krishnashrivastava23@gmail.com>
2025-03-20 21:00:09 +0530

    Refactor challenge1 API to use userId for fetching user and generating challenge

--- src/app/api/challenge1/route.ts
+++ src/app/api/challenge1/route.ts
@@ -1,15 +1,22 @@
+import connectToDatabase from "@/db/db";
 import { generateChallengeForUser } from "@/lib/encryption";
-import { NextRequest,NextResponse } from "next/server";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
 
 
 export async function POST(req: NextRequest) {
+    await connectToDatabase();
     const body = await req.json();
-    const {username} = body;
-    if (!username) {
+    const { userId } = body;
+    if (!userId) {
         return NextResponse.json({ message: "Username is required" }, { status: 400 });
     }
     try {
-        const encryptedFlag = generateChallengeForUser(username);
+        const user = await User.findById(userId);
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        const encryptedFlag = generateChallengeForUser(user.username);
         return NextResponse.json({
             encryptedFlag,
             message: "Key to decrypt the message is derived from username",
